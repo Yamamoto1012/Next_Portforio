87c987bc6148252f50d0d089f305a6aa7b9a804b
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-04-06 00:15:48 +0900

    Header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,16 +4,32 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <nav>
-        <div>
-        </div>
-        <ul>
-          <li><Link href="/product">product</Link></li>
-          <li><Link href="/history">history</Link></li>
-          <li><Link href="/about">about</Link></li>
-          <li><Link href="/blog">blog</Link></li>
+      <nav className="flex items-center justify-between p-2 w-full rounded-2xl bg-[#2C3333]">
+        <Link href="/" className="hover:scale-105 duration-150">
+          <Image
+            src="/icon.jpeg"
+            alt="ロゴ"
+            width={60}
+            height={60}
+            className=" rounded-full"
+          />
+        </Link>
+
+        <ul className="flex gap-3 mr-8 text-base text-white ">
+          <li className="hover:border-b duration-75 p-1">
+            <Link href="/product">product</Link>
+          </li>
+          <li className="hover:border-b duration-75 p-1">
+            <Link href="/history">history</Link>
+          </li>
+          <li className="hover:border-b duration-75 p-1">
+            <Link href="/about">about</Link>
+          </li>
+          <li className="hover:border-b duration-75 p-1">
+            <Link href="/blog">blog</Link>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
